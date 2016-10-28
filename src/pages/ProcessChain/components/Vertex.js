4886f8593394f7b776be43ef08492e5d57cb5b95
cloud chan <cloud.cck@gmail.com>
2016-10-28 15:45:42 +0800

    add VertexFn

--- src/pages/ProcessChain/components/Vertex.js
+++ src/pages/ProcessChain/components/Vertex.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import VertexFn from './VertexFn';
+
 class Vertex extends Component {
   constructor(props) {
     super(props);
@@ -7,39 +9,42 @@ class Vertex extends Component {
   }
 
   getParent(event) {
-    console.log('getParent:',this.props.data.id);
-    this.props.getParent('aaa','bbb',this.props.data.id);
+    console.log('getParent:', this.props.data.id);
+    this.props.getParent('aaa', 'bbb', this.props.data.id);
   }
   getChild(event) {
     console.log('getChild:', this.props.data.id);
-    this.props.getChild('ccc','ddd',this.props.data.id);
+    this.props.getChild('ccc', 'ddd', this.props.data.id);
+  }
+  toggleSize(id) {
+    console.log('toggle size of ', id);
+  }
+  toggleHidden(id) {
+    console.log('toggle hidden of ', id);
   }
 
   render() {
-    const {label, width: w, height: h, x, y} = this.props.data;
-    const x0 = x - w / 2;
-    const x1 = x + w / 2;
-    const y0 = y - h / 2;
-    const y1 = y + h / 2;
     const fileColor = _.includes(['_D', 'K', 'L', 'W', 'X', 'Z'], this.props.data.id) ? 'red' : 'gray';
+    const {label, width: w, height: h, x, y, id} = this.props.data;
+    let [x0, x1, y0, y1] = [x - w / 2, x + w / 2, y - h / 2, y + h / 2];
+
     const radius = 10;
+    const functions = [
+      { label: 'p', color: 'red', bindFn: this.getParent },
+      { label: 'c', color: 'green', bindFn: this.getChild },
+      { label: 's', color: 'yellow', bindFn: this.toggleSize },
+      { label: 'h', color: 'gray', bindFn: this.toggleHidden }
+    ];
+
     return (
       <g className="graph-vetex">
-        <g className="" onClick={(e) => { this.getParent(e); } } >
-          <rect x={x0-10} y={y0} width={10} height={10} fill="green"/>
-          <text className="vertex-title" textAnchor="middle" alignmentBaseline="central" x={x0-5} y={y0+5}>+</text>
-        </g>
         <g>
           <rect x={x - w / 2} y={y - h / 2} width={w} height={h} rx={radius} ry={radius} fill={fileColor} />
           <text className="vertex-title" textAnchor="middle" alignmentBaseline="central" x={x} y={y}>{label}</text>
         </g>
-        <g onClick={(e) => { this.getChild(e); } } >
-          <rect x={x1} y={y0} width={10} height={10} fill="yellow" />
-          <text className="vertex-title" textAnchor="middle" alignmentBaseline="central" x={x1+5} y={y0+5}>+</text>
-        </g>
+        {functions.map(f => <VertexFn key={f.label} setting={Object.assign({}, f, { x: x0 += 10, y: y0 - 10, w: 10, h: 10, vertexId: id })} />)}
       </g>
     );
   }
 }
-
 export default Vertex;
---
+++ src/pages/ProcessChain/components/VertexFn.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class VertexFn extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const {x, y, w, h, label, color, vertexId} = this.props.setting;
+    return (
+      <g onClick={() => this.props.setting.bindFn(vertexId)}>
+        <rect x={x} y={y} width={w} height={h} fill={color} />
+        <text className="vertex-title" textAnchor="middle" alignmentBaseline="central" x={x + w / 2} y={y + h / 2}>{label}</text>
+      </g>
+    );
+  }
+}
+
+export default VertexFn;
