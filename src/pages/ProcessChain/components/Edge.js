cbfa2ef651f53baac026e5cfb09d94ba2c370f90
cloud chan <cloud.cck@gmail.com>
2016-10-28 10:20:29 +0800

    update Edge

--- src/pages/ProcessChain/components/Edge.js
+++ src/pages/ProcessChain/components/Edge.js
@@ -5,18 +5,17 @@ class Edge extends Component {
   constructor(props) {
     super(props);
   }
-  
-
+  createTextPath(hrefId, dy, label) {
+    return `<textPath href="#${hrefId}" direction="ltr" startOffset="50%"><tspan dy=${dy}>${label}</tspan></textPath>`;
+  }
   render() {
-    const {id, oper, time, points} = this.props.data;
-    const timeLabel = moment.unix(time).format('YY/MM/DD | hh:mm:ss');
+    const {id, oper, time, points, dest: {y: minY}} = this.props.data;
+    const operTextPath = this.createTextPath(id, -3, oper);
+    const timeTextPath = this.createTextPath(id, 10, moment.unix(time).format('YY/MM/DD | hh:mm'));
     const curve = points
       .map(p => `${p.x} ${p.y}`)
-      .reduce((pre, curr, index, arr) => index % 2 ? `${pre} Q ${curr}` : `${pre} ${curr}`, 'M');
-    let operTextPath = `<textPath href="#${id}" direction="ltr" startOffset="50%"><tspan dy="-5">${oper}</tspan></textPath>`;
-    let timeTextPath = `<textPath href="#${id}" direction="ltr" startOffset="50%"><tspan dy="15">${timeLabel}</tspan></textPath>`;
+      .reduce((pre, curr, i) => i % 2 ? `${pre} Q ${curr}` : `${pre} ${curr}`, 'M');
 
-    // <text dangerouslySetInnerHTML={{ __html: textPath }}></text>;
     return (
       <g className="graph-edge">
         <path id={id} d={curve} fill="transparent" stroke="#000000" strokeWidth="1" ></path>
